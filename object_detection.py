0016884b260d10398e4ec9fa9418a9dd56d442a3
Ted Carlson <tedcarlson2019@u.northwestern.edu>
2020-07-27 23:40:19 -0500

    Improved model. Connected Twilio

---
+++ object_detection.py
@@ -0,0 +1,25 @@
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request, redirect, send_from_directory
+import requests
+import gpt_2_simple as gpt2
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms():
+    print('here')
+    resp = MessagingResponse()
+    Prefix = request.values['Body']
+    print(Prefix)
+    sess = gpt2.start_tf_sess()
+    gpt2.load_gpt2(sess)
+    resp_str = str(gpt2.generate(sess,
+                length=50,
+                temperature=.8,
+                prefix=Prefix,
+                top_p=0.9,
+                nsamples=1,
+                return_as_list=True)[0])
+    print(resp_str)
+    resp.message(resp_str)
+    return str(resp)
